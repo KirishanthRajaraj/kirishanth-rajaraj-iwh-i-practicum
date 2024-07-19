468ba458abcaf386e9dc0941585059346f8b6db4
KirishanthRajaraj <KirishanthRajaraj@NOTEBOOK-KIRI>
2024-07-19 12:09:43 +0200

    added update custom object post request, with a form that uses it

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const axios = require('axios');
+require("dotenv").config();
+
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -11,19 +13,48 @@ app.use(express.json());
 const PRIVATE_APP_KEY = process.env.PRIVATE_APP_KEY;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
+  res.send("home");
+  console.log(PRIVATE_APP_KEY);
 });
 // * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-app.get('/update-cobj', async (req, res) => {
-    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum'});
-
+app.get("/update-cobj", async (req, res) => {
+  res.render("updates", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+  });
 });
 // * Code for Route 2 goes here
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-app.post('/update', async (req, res) => {
+app.post("/update", async (req, res) => {
+  const ologyType = "2-131427213";
+  const customObjUrl = "https://api.hubapi.com/crm/v3/objects/" + ologyType;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_KEY}`,
+    "Content-Type": "application/json",
+  };
+
+  const update = {
+    properties: {
+      name: req.body.name,
+      description: req.body.description,
+      years_to_study: req.body.years_to_study,
+    },
+  };
+
+  try {
+    const response = await axios.post(customObjUrl, update, { headers });
+    const data = response.data.results;
+    if (response.status === 200 || response.status === 201) {
+      res.redirect("/");
+    } else {
+      throw new Error(`Object could not be created ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
 // * Code for Route 3 goes here
 
@@ -71,6 +102,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
